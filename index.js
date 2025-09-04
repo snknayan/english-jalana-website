f1f263daa7857719af3fa523f0ed19d1ef100616
Nayan <snknayan01995@example.com>
2025-09-04 20:37:15 +0600

    speaker added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-US"; // or "en-GB"
+  window.speechSynthesis.speak(utterance);
+}
+
 const createElement = (arr) => {
   const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
   return htmlElements.join(" ");
@@ -142,7 +148,9 @@ const displayLevelWord = (words) => {
           })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
             <i class="fa-solid fa-circle-info"></i>
           </button>
-          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+          <button onclick="pronounceWord('${
+            word.word
+          }')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
             <i class="fa-solid fa-volume-high"></i>
           </button>
         </div>
